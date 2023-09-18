db022b62b51c47b309a23904be698ba67f7ed75f
BLACKMouha <mohamedsenghor05@gmail.com>
2023-09-18 12:08:02 +0000

    Setting Babel languages and timezones

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''1-app module'''
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, get_locale, get_timezone
+from pytz import utc
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    '''Config representation for setting a Flask app'''
+    LANGUAGES = ['en', 'fr']
+    default_langague = 'en'
+    default_timezone = 'UTC'
+
+
+config = Config()
+app.config['BABEL_DEFAULT_LOCALE'] = config.default_langague
+app.config['BABEL_DEFAULT_TIMEZONE'] = config.default_timezone
+
+
+@app.before_request
+def before_any_request():
+    '''Called before a request is handled'''
+    g.locale = str(get_locale())
+    g.timezone = str(get_timezone())
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''Handle root route'''
+    return render_template('1-index.html'), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
